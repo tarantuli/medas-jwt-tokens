dddcb3e7beb2abfbbbfdb7c38167e91837916e77
Patrick Morpey <patrick@morphp.nl>
2026-05-05 09:40:22 +0200

    Add caching support for JWT token decoding in `JwtAuthTokenController`.

--- src/JwtAuthTokenController.php
+++ src/JwtAuthTokenController.php
@@ -9,7 +9,8 @@ use Medas\Core\{
     Attributes\ConfigValue,
     Attributes\Service,
     Interfaces\AuthenticationData,
-    Interfaces\AuthenticationTokenController
+    Interfaces\AuthenticationTokenController,
+    Interfaces\CacheManager
 };
 use Medas\ObjectToArraySerializer\ObjectToArraySerializer;
 
@@ -23,18 +24,19 @@ readonly class JwtAuthTokenController implements AuthenticationTokenController
         #[ConfigValue(ConfigOptions\JwtAuthTokenAlgorithm::class)]
         private string                  $algorithm,
         private ObjectToArraySerializer $serializer,
+        private CacheManager            $cacheManager,
     )
     {
     }
 
     public function create(AuthenticationData $data): string
     {
-        $payLoad = [
+        $payload = [
             'data' => $this->serializer->serialize($data),
             'class' => $data::class,
         ];
 
-        return JWT::encode($payLoad, $this->tokenKey, $this->algorithm);
+        return JWT::encode($payload, $this->tokenKey, $this->algorithm);
     }
 
     public function invalidate(string $token): void
@@ -44,10 +46,38 @@ readonly class JwtAuthTokenController implements AuthenticationTokenController
 
     public function data(string $token): AuthenticationData|null
     {
-        $payload = JWT::decode($token, new Key($this->tokenKey, $this->algorithm));
+        return $this->cacheManager->get('memory')->get(
+            'jwt-tokens:data:' . sha1($token),
+            fn() => $this->decode($token)
+        );
+    }
+
+    private function decode(string $token): AuthenticationData|null
+    {
+        try {
+            $payload = JWT::decode($token, new Key($this->tokenKey, $this->algorithm));
+        }
+        catch (\Exception) {
+            return null;
+        }
+
+        // Check that the class name is an instance of AuthenticationData.
+        /** @var string $class */
+        $class = $payload->class;
+
+        if (!is_a($class, AuthenticationData::class, true)) {
+            return null;
+        }
+
+        // Cast stdClass object to array so the ObjectToArraySerializer can unserialize it.
+        $data = json_decode(json_encode($payload->data), true);
+
+        if ($data === null) {
+            return null;
+        }
 
         /** @var AuthenticationData */
-        return $this->serializer->unserialize($payload->data, class: $payload->class);
+        return $this->serializer->unserialize($data, class: $class);
     }
 
     public function userId(string $token): mixed
