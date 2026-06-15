7149c8281039b2aaeb347ae45b15a990c9ec636b
Patrick Morpey <patrick@morphp.nl>
2026-06-15 12:45:58 +0200

    Update `composer.json` and `composer.lock` to upgrade `medas-core` and related dependencies to v4. Update PHPUnit bootstrap for `ServiceConfigBuilder` usage.

--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 use Medas\JwtTokens\JwtTokensPackage;
 use Medas\ObjectInstantiator\ObjectInstantiator;
 use Medas\RamseyUuidBridge\RamseyUuidBridgePackage;
-use Medas\ServiceManager\{ServiceConfig, ServiceManager};
+use Medas\ServiceManager\{ServiceConfigBuilder, ServiceManager};
 
 chdir(__DIR__);
 
-new ServiceManager(function (): ServiceConfig {
-    $config = new ServiceConfig(ObjectInstantiator::class);
+new ServiceManager(function (): ServiceConfigBuilder {
+    $config = new ServiceConfigBuilder(ObjectInstantiator::class);
 
     $config->addPackages([
         JwtTokensPackage::instance(),
